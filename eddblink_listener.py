cc534d852fba0d398fefd10e3eb981c3b54e11ea
eyeonus <eyeonus@gmail.com>
2020-06-30 22:59:31 -0600

    refactor: Remove unneeded newline.

--- eddblink_listener.py
+++ eddblink_listener.py
@@ -628,7 +628,7 @@ def process_messages():
             system_id = system_ids.get(system)
             if not system_id:
                 system_id = 0
-                print("\nSystem not found, adding with default values.")
+                print("System not found, adding with default values.")
                 success = False
                 while not success:
                     try:
